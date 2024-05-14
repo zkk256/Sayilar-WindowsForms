16295ede2fcf8b76f49f994108fb9cdec7fefc3d
Kutay <ortak.n.2001@gmail.com>
2024-05-14 21:35:21 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnArkadas = new System.Windows.Forms.Button();
+            this.btnSon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(27, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(27, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(76, 50);
+            this.txtSayi1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(110, 23);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(76, 107);
+            this.txtSayi2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(110, 23);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // btnArkadas
+            // 
+            this.btnArkadas.AutoSize = true;
+            this.btnArkadas.Location = new System.Drawing.Point(27, 178);
+            this.btnArkadas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnArkadas.Name = "btnArkadas";
+            this.btnArkadas.Size = new System.Drawing.Size(88, 25);
+            this.btnArkadas.TabIndex = 4;
+            this.btnArkadas.Text = "Arkadaş mı?";
+            this.btnArkadas.UseVisualStyleBackColor = true;
+            this.btnArkadas.Click += new System.EventHandler(this.btnArkadas_Click);
+            // 
+            // btnSon
+            // 
+            this.btnSon.Location = new System.Drawing.Point(120, 178);
+            this.btnSon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSon.Name = "btnSon";
+            this.btnSon.Size = new System.Drawing.Size(82, 22);
+            this.btnSon.TabIndex = 5;
+            this.btnSon.Text = "SON";
+            this.btnSon.UseVisualStyleBackColor = true;
+            this.btnSon.Click += new System.EventHandler(this.btnSon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 227);
+            this.Controls.Add(this.btnSon);
+            this.Controls.Add(this.btnArkadas);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnArkadas;
+        private System.Windows.Forms.Button btnSon;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,140 @@
+﻿/****************************************************************************************
+**					     SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				     BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				    NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					     2020-2021 YAZ DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........:1. Ödev
+**				ÖĞRENCİ ADI............:Ali Kutay KILINÇ
+**				ÖĞRENCİ NUMARASI.......:b201210071
+**              DERSİN ALINDIĞI GRUP...:1. Öğretim A
+*****************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //Butona(Arkadas mı?) basılma sayısınıu gösteren değer
+        int basildi = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArkadas_Click(object sender, EventArgs e)
+        {
+            //Butona(Arkadas mı?) 2. kez basıldığında bir daha basılaması engellenir
+            basildi++;
+            if (basildi == 2)
+            {
+                btnArkadas.Enabled = false;
+            }
+            else
+            {
+                //Butona basıldıktn sonra form bu boyutlara büyür
+                this.Height = 440;
+                this.Width = 600;
+
+                //Program ilk başladığında görünenler dışında ekrana yeni eklenecek kontroller tanımlanır
+                var Liste1 = new ListBox();
+                var Liste2 = new ListBox();
+                var lblX = new Label();
+                var lblY = new Label();
+                var txtXToplam = new TextBox();
+                var txtYToplam = new TextBox();
+
+                Liste1.Top = 50;
+                Liste2.Top = 50;
+                Liste1.Left = 275;
+                Liste2.Left = 425;
+                Liste1.Height = 250;
+                Liste2.Height = 250;
+
+                lblX.Text = "X";
+                lblY.Text = "Y";
+                lblX.Left = 325;
+                lblY.Left = 475;
+                lblX.Top = 20;
+                lblY.Top = 20;
+
+
+                //Çarpanların toplam değerleri TextBox'larda kullanıcının değiştirmesine izin verilmeyecek şekilde olacak.
+                txtXToplam.ReadOnly = true;
+                txtYToplam.ReadOnly = true;
+
+                txtXToplam.AutoSize = false;
+                txtYToplam.AutoSize = false;
+
+                txtXToplam.BackColor = Color.White;
+                txtYToplam.BackColor = Color.White;
+                txtXToplam.Left = 275;
+                txtYToplam.Left = 425;
+                txtXToplam.Top = 300;
+                txtYToplam.Top = 300;
+
+                //Yeni kontroller eklenir
+                Controls.Add(lblX);
+                Controls.Add(lblY);
+
+                Controls.Add(txtXToplam);
+                Controls.Add(txtYToplam);
+
+                Controls.Add(Liste1);
+                Controls.Add(Liste2);
+
+                //Girilen iki sayı int olarak alınır
+                int x = int.Parse(txtSayi1.Text);
+                int y = int.Parse(txtSayi2.Text);
+                int xToplam = 0, yToplam = 0;
+
+                //X sayısı çarpanlarına ayrılır
+                //X sayısının her bir çarpanı Liste1'e eklenir. 
+                for (int i = 1; i < x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        xToplam += i;
+                        Liste1.Items.Add(i.ToString());
+                    }
+                }
+
+                //Y sayısı çarpanlarına ayrılır
+                //Y sayısının her bir çarpanı Liste2'ye eklenir. 
+                for (int j = 1; j < y; j++)
+                {
+                    if (y % j == 0)
+                    {
+                        yToplam += j;
+                        Liste2.Items.Add(j.ToString());
+                    }
+                }
+                txtXToplam.Text = xToplam.ToString();
+                txtYToplam.Text = yToplam.ToString();
+
+                //Sayıların arkadaş olup olmadığı kontrol edilir. Mesaj olarak ekrana gelir
+                if (xToplam == y && yToplam == x)
+                    MessageBox.Show("Arkadaş Sayılar");
+                else
+                    MessageBox.Show("Arkadaş değiller");
+            }
+        }
+
+        //Bu buton(SON) programı kapatır
+        private void btnSon_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
